478db2e156c54a4709311f11f0e072cc7e143bef
Adarsh <singhadarshkr836@gmail.com>
2025-11-25 03:32:38 +0530

    Project structure setup

---
+++ src/com/onlinefooddelivery/main/Main.java
@@ -0,0 +1,7 @@
+package com.onlinefooddelivery.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Online Food Delivery System Started...");
+    }
+}
